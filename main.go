9ccbd283e95ce782a1e01f4cd9313b73e3656257
Alexandre F <alexandretnparaujo@outlook.com>
2026-05-15 20:46:15 +0200

    adding logging (zap)

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/fernale/crud-go/src/configuration/logger"
 	"github.com/fernale/crud-go/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main(){
+	logger.Info("About to start the application")
 	err:= godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,25 +1,28 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/fernale/crud-go/src/configuration/logger"
 	"github.com/fernale/crud-go/src/configuration/validation"
 	"github.com/fernale/crud-go/src/controller/model/request"
 	"github.com/fernale/crud-go/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context){
+	logger.Info("Init CreateUser controller", zap.String("journey", "createUser"))
+	
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err)
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
 	
 	response := response.UserResponse{
 		ID: "test",
@@ -27,5 +30,7 @@ func CreateUser(c *gin.Context){
 		Email: userRequest.Email,
 		Age: userRequest.Age,
 	}
+
+	logger.Info("User CreateUser successfully", zap.String("journey", "createUser"))
 	c.JSON(http.StatusCreated, response)
 }
